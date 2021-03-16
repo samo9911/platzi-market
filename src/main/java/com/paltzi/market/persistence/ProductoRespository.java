9411cd2a91335a71bc9e40ec26140d53efd19a48
Samuel <jos90500@gmail.com>
2021-03-15 22:23:10 -0600

    actualizacion 4
    no se me ocurre nada

--- src/main/java/com/paltzi/market/persistence/ProductoRespository.java
+++ src/main/java/com/paltzi/market/persistence/ProductoRespository.java
@@ -3,11 +3,20 @@ package com.paltzi.market.persistence;
 import com.paltzi.market.persistence.entity.Producto;
 import com.paltzi.market.persistence.crud.ProductoCrudRepository;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRespository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria){
+       return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, estado:true);
     }
 }
